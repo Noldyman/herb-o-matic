cf0930bd2e82d67798bb501b8377615b83ee0d6f
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-02-20 19:14:25 +0100

    update for first physical prototype

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ---
 
-Automatic watering system for my precious herbs.
+The Herb-O-Matic is an automatic watering system for my precious basil and parsley plants. For this project I used a Wemos D1 mini. All the hardware was orderd at https://www.tinytronics.nl.
+
+Every hour, the system measures the moistness of the soil in the planter. If the moistness is too low, it will pump water into the planter. After it performed these actions, it will send the data (soil moistness and weather the pump was active) to my home-assistant server over MQTT.
--- herb-o-matic.ino
+++ herb-o-matic.ino
@@ -11,13 +11,43 @@ const char* mqtt_pwd = CRED_MQTT_PASSWD;
 const char* clientId = CRED_CLIENT_ID;
 const char* pub_topic = CRED_PUB_TOPIC;
 
-#define SOILMOISTPIN A0
+#define soilMoistPin A0
+#define powerLedPin D5
+#define errorLedPin D2
+#define pumpPin D1
 
-uint32_t averageSoilMoistValue;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int averageSoilMoistness;
+boolean pumpWasActive = false;
+
+void successBlink(){
+   digitalWrite(powerLedPin, LOW);
+   delay(100);
+   digitalWrite(powerLedPin, HIGH);
+}
+
+void errorBlink(int numOfBlinks){
+  for (int i = 0; i < numOfBlinks; i++) {
+    digitalWrite(errorLedPin, HIGH);
+    delay(100);
+    digitalWrite(errorLedPin, LOW);
+    if(i != numOfBlinks - 1){
+      delay(100);  
+    }
+  }
+}
+
+void handleError(char* errorText){
+  Serial.print("Error: ");
+  Serial.print(errorText);
+  errorBlink(10);
+}
 
 void connectWifi(){
   Serial.println();
-  Serial.print("Connecting to: ");
+  Serial.print("Connecting to ");
   Serial.println(ssid);
   
   WiFi.begin(ssid, password);
@@ -31,53 +61,99 @@ void connectWifi(){
   if(WiFi.status() != WL_CONNECTED){
     handleError("Failed to connect to Wifi.");
   } else{
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();  
+    Serial.println();
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: "+ WiFi.localIP().toString() + ".");
+    successBlink();
   }
 }
 
-uint32_t measureSoilMoistPercentage(){
-  return constrain(map(analogRead(SOILMOISTPIN), 640, 300, 0, 100), 0, 100);  
+int measureSoilMoistPercentage(){
+  return constrain(map(analogRead(soilMoistPin), 640, 230, 0, 100), 0, 100);  
 }
 
-void calculateAverageSoilMoistPercentage(){
-  uint32_t value1 = measureSoilMoistPercentage();
-  delay(1000);
-  uint32_t value2 = measureSoilMoistPercentage();
-  delay(1000);
-  uint32_t value3 = measureSoilMoistPercentage();
-  averageSoilMoistValue = (value1+value2+value3)/3;
-}
-
-void pumpWater(uint32_t pumpTime){
-  Serial.println("Started pumping water.");
-  digitalWrite(D1,HIGH);
-  delay(pumpTime);
-  Serial.println("Stopped pumping water");
-  digitalWrite(D1,LOW);
-
-}
-
-void handleError(char* errorText){
+void calculateAverageSoilMoistness(int numOfMeasurements, int timeBetweenMeasurementsInMS){
   Serial.println();
-  Serial.println("Error: ");
-  Serial.print(errorText);
+  Serial.println("Calculating avarage soil moist percentage.");
+
+  int sumOfMeasurements = 0;
+
+  for (int i = 0; i < numOfMeasurements; i++) {
+    sumOfMeasurements += measureSoilMoistPercentage();
+    if(i != numOfMeasurements - 1){
+      delay(timeBetweenMeasurementsInMS);
+    }
+  }
+
+  averageSoilMoistness = sumOfMeasurements / numOfMeasurements;
+
+  Serial.println();
+  Serial.println("Average soil moistness: " + String(averageSoilMoistness) + " %.");
+  successBlink();
+}
+
+void pumpWater(int pumpTimeInS){
+  Serial.println();
+  Serial.println("Started pumping water.");
+  digitalWrite(pumpPin,HIGH);
+  delay(pumpTimeInS * 1000);
+  Serial.println("Stopped pumping water");
+  digitalWrite(pumpPin,LOW);
+}
+
+void decideToPump(){
+  if(averageSoilMoistness < 30){
+    pumpWater(60);
+    pumpWasActive = true;
+  }else{
+    Serial.println();
+    Serial.println("No need to pump water.");
+  }
+}
+
+void publishData(){
+  Serial.println();
+  Serial.println("Connecting to MQTT server...");
+  if (client.connect(clientId, mqtt_user, mqtt_pwd)) {
+    Serial.println("MQTT connected");
+    successBlink();
+    
+    char payload[40];
+    sprintf(payload, "{\"soilMoistMoistness\": %i, \"pumpWasActive\": %s}", averageSoilMoistness, pumpWasActive ? "true" : "false");
+    boolean published = client.publish(pub_topic, payload);
+    delay(500);
+    
+    if(published){
+    Serial.println();
+    Serial.println("Planter data was published:");
+    Serial.println(payload); 
+    successBlink();
+  } else{
+    handleError("Failed to publish planter data");
+  }
+  } else {
+    handleError("Failed to connect to MQTT server.");
+  }
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(D1,OUTPUT);
+  pinMode(D0, WAKEUP_PULLUP);
+  pinMode(pumpPin,OUTPUT);  
+  pinMode(powerLedPin,OUTPUT);
+  pinMode(errorLedPin,OUTPUT);
   delay(200);
+
+  digitalWrite(powerLedPin,HIGH);
   
   connectWifi();
-  calculateAverageSoilMoistPercentage();
-  Serial.println("Average soil moistness: ");
-  Serial.print(averageSoilMoistValue);
-
-  // pumpWater(5000);
+  client.setServer(mqtt_server, mqtt_port);
+  
+  calculateAverageSoilMoistness(5, 200);
+  decideToPump();
+  publishData();
+ 
+  ESP.deepSleep(1 * 60 * 6e7);
 }
 
 void loop() {
